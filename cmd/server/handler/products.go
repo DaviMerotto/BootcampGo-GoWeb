74cbae97d0fe48f2a2a64771fe1264ba9f061c84
Davi Vincenzo Merotto <davi.merotto@mercadolivre.com>
2024-04-11 11:00:31 -0300

    feat: Day 3 morning exercise

--- cmd/server/handler/products.go
+++ cmd/server/handler/products.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/davimerotto/web-server/internal/products"
+	"github.com/davimerotto/web-server/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,9 +23,8 @@ func (c *ProductHandler) GetAll() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		p, err := c.service.GetAll()
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
 
@@ -33,7 +33,7 @@ func (c *ProductHandler) GetAll() gin.HandlerFunc {
 			return
 		}
 
-		ctx.JSON(http.StatusOK, p)
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, p, ""))
 	}
 }
 
@@ -41,18 +41,18 @@ func (c *ProductHandler) Create() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var req products.Product
 		if err := ctx.Bind(&req); err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-				"error": err.Error(),
-			})
+			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity,
+				web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 			return
 		}
 
 		p, err := c.service.Create(req)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
-		ctx.JSON(http.StatusCreated, p)
+		ctx.JSON(http.StatusCreated, web.NewResponse(http.StatusCreated, p, ""))
 	}
 }
 
@@ -60,15 +60,18 @@ func (c *ProductHandler) Delete() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		id, err := strconv.ParseUint(ctx.Param("id"), 10, 0)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
 		err = c.service.Delete(uint(id))
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
-		ctx.JSON(http.StatusAccepted, gin.H{"message": "Produto deletado com sucesso"})
+		ctx.JSON(http.StatusAccepted,
+			web.NewResponse(http.StatusAccepted, "produto deletado com sucesso!", ""))
 	}
 }
 
@@ -76,44 +79,53 @@ func (c *ProductHandler) UpdateFull() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		p := products.Product{}
 		if err := ctx.Bind(&p); err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity,
+				web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 			return
 		}
 		if p.Id == 0 {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "ID não informado"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "ID não informado"))
 			return
 		}
 		if p.Name == "" {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Nome não informado"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "Nome não informado"))
 			return
 		}
 		if p.Color == "" {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Cor não informada"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "Cor não informada"))
 			return
 		}
 		if p.Price == 0 {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Preço não informado"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "Preço não informado"))
 			return
 		}
 		if p.Stock == 0 {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Estoque não informado"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "Estoque não informado"))
 			return
 		}
 		if p.Code == "" {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Código não informado"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "Código não informado"))
 			return
 		}
 		if p.Creation_date == "" {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Data de criação não informada"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "Data de criação não informada"))
 			return
 		}
 
 		prod, err := c.service.UpdateFull(p)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
-		ctx.JSON(http.StatusOK, prod)
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, prod, ""))
 	}
 }
 
@@ -122,22 +134,26 @@ func (c *ProductHandler) Update() gin.HandlerFunc {
 		p := products.Product{}
 		id, err := strconv.ParseUint(ctx.Param("id"), 10, 0)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity,
+				web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 			return
 		}
 		if err := ctx.Bind(&p); err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity,
+				web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 			return
 		}
 		if id == 0 {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "ID não informado"})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, "ID não informado"))
 			return
 		}
 		prod, err := c.service.Update(uint(id), p)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 			return
 		}
-		ctx.JSON(http.StatusOK, prod)
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, prod, ""))
 	}
 }
